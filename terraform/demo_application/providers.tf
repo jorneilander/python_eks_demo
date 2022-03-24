fb7d170694d93c6a69e395e53c730cc78ea5d805
Jorn Eilander <jorn.eilander@azorion.com>
2022-03-24 15:16:56 +0100

    cleanup Terraform demo application deployment

--- terraform/demo_application/providers.tf
+++ terraform/demo_application/providers.tf
@@ -11,7 +11,7 @@ terraform {
     }
 
     docker = {
-      source = "kreuzwerker/docker"
+      source  = "kreuzwerker/docker"
       version = ">= 2.16.0"
     }
   }
@@ -34,9 +34,25 @@ provider "kubernetes" {
 
 provider "docker" {
   registry_auth {
-    # address  = data.aws_ecr_repository.image_registry.repository_url
-    address = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.terraform_remote_state.eks.outputs.region}.amazonaws.com"
+    address  = "${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.terraform_remote_state.eks.outputs.region}.amazonaws.com"
     username = data.aws_ecr_authorization_token.demo_application_registry_token.user_name
     password = data.aws_ecr_authorization_token.demo_application_registry_token.password
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      command     = "aws"
+      args = [
+        "eks",
+        "get-token",
+        "--cluster-name",
+        data.aws_eks_cluster.cluster.id
+      ]
+    }
   }
 }
